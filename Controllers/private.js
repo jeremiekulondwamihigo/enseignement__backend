0f86768642638a09f4fc7fabe83a26463c1d4d9f
jeremiekulondwamihigo <grace1mihigo@gmail.com>
2022-08-07 13:02:40 +0200

    commissionREle

--- Controllers/private.js
+++ Controllers/private.js
@@ -5,7 +5,7 @@ getPrivateData : (req, res, next)=>{
 
     res.status(200).json({
         success : true,
-        data : "You got access to the private data in this route"
+        data : "Token expirer"
     })
 }
 }
